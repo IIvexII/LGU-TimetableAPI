7a779e42b4dbc05653bb4a4e9aa567d4547990d6
IIvexII <mrh4ck3r1738@gmail.com>
2022-10-21 11:26:11 +0500

    Added Removed unnecessary headers properties

--- src/Sync.js
+++ src/Sync.js
@@ -36,27 +36,7 @@ class Sync {
     const config = {
       headers: {
         Cookie: `PHPSESSID=${this.sessionId}`,
-        // Remove the https:// from the baseurl
-        Host: baseUrl.slice(8),
-        'Cache-Control': 'max-age=0',
-        'Sec-Ch-Ua': '"Not;A=Brand";v="99", "Chromium";v="106"',
-        'Sec-Ch-Ua-Mobile': '?0',
-        'Sec-Ch-Ua-Platform': 'Linux',
-        'Upgrade-Insecure-Requests': '1',
-        Origin: baseUrl,
         'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.5249.62 Safari/537.36',
-        Accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-User': '?1',
-        'Sec-Fetch-Dest': 'document',
-        Referer: url,
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.9',
-        Connection: 'close',
       },
     };
 
