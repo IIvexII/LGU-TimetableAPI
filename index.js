214d170f256d166af4e9b56fb5eddf2a2e68d34a
IIvexII <mrh4ck3r1738@gmail.com>
2022-10-24 20:29:02 +0500

    Fixed error

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const Timetable = require('./modules/Timetable');
 // Middlewares
-const { Session, Params } = require('./middlewares');
+const { Session, Params } = require('./src/middlewares');
 
-const { Timetable } = require('./models');
+const { Timetable } = require('./src/models');
 
 const app = express();
 
-/* ------------------------------------
- * Route: '/'
- *
- * Middlewares:
- *  1. Session.validate
- *  2. Params.validate
- *
- * Query:
- *  1. session  2. semester,
- *  3. program  4. section
- * ------------------------------------
- */
 app.get('/', Session.validate, Params.validate, async (req, res) => {
   const { session, semester, program, section } = req.data;
 
