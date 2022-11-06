258eecd6eef1d71bbc68af82a52b4185129b9cee
IIvexII <mrh4ck3r1738@gmail.com>
2022-11-06 19:36:47 +0500

    Deploy

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+require('dotenv').config();
 const express = require('express');
-const { Session, Params } = require('./src/middlewares');
+const { Section } = require('./src/models');
 
-const LGURouter = require('./src/routes/LGURouter');
-const GCRouter = require('./src/routes/GCRouter');
+const { PopulateRouter, LGURouter, GCRouter } = require('./src/routes');
 
 const app = express();
-// Applying Middlewares to all routes
-app.use(Session.validate, Params.validate);
 
 app.use(GCRouter);
 app.use(LGURouter);
+app.use(PopulateRouter);
+
+app.get('/meta', async (req, res) => {
+  const sections = await Section.find({});
+  const arr = [];
+
+  for (let section of sections) {
+    arr.push({
+      semester: {
+        _id: section.degree.semester._id,
+        name: section.degree.semester.name,
+      },
+      degree: {
+        _id: section.degree.degreeId,
+        name: section.degree.degreeName,
+      },
+      section: {
+        _id: section.sectionId,
+        name: section.sectionTag,
+      },
+    });
+  }
+  res.send(arr);
+});
 
 module.exports = app;
