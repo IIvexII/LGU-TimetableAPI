02829520b116767ce923a966da88a205ce8c6b9d
Zain ul din <78583049+Zain-ul-din@users.noreply.github.com>
2022-11-09 21:08:30 +0500

    added cors and prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors    = require ('cors');
 
 const { PopulateRouter, TimetableRouter, GCRouter } = require('./routes');
 
 const app = express();
 
-app.use(GCRouter);
-app.use(TimetableRouter);
-app.use(PopulateRouter);
+app
+.use (cors ({})) // allow all
+.use(GCRouter)
+.use(TimetableRouter)
+.use(PopulateRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
