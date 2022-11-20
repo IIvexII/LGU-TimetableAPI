1925e6d9b210febb6a30e2db95f27d981a3a31e8
IIvexII <mrh4ck3r1738@gmail.com>
2022-11-20 18:28:41 +0500

    feat: get free room at specific time slot

--- src/routes/RoomRouter.js
+++ src/routes/RoomRouter.js
@@ -7,5 +7,6 @@ const router = Router();
 router.get('/days', RoomController.getDays);
 router.get('/rooms', RoomController.getRooms);
 router.get('/allFreeRooms', RoomController.getFreeRooms);
+router.get('/freeRooms', RoomController.getSpecificFreeRooms);
 
 module.exports = router;
