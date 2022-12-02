b610887fc9cd649ba4a4f38f18e7a0e45eb489c0
IIvexII <mrh4ck3r1738@gmail.com>
2022-12-02 18:33:35 +0500

    refactor: rename all the router files and removed the Router their names

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
-const GCRouter = require('./GCRouter');
-const TimetableRouter = require('./TimetableRouter');
-const PopulateRouter = require('./PopulateRouter');
-const RoomRouter = require('./RoomRouter');
+const GCRouter = require('./GoogleCalendar');
+const TimetableRouter = require('./Timetable');
+const PopulateRouter = require('./Populate');
+const RoomRouter = require('./Room');
 
 module.exports = {
   GCRouter,
